d326202a9ced1ed0f13566549f45825a4c134e42
Canter-Cycles <calebvweich@gmail.com>
2025-09-23 17:55:32 +0100

    Added dialog component

---
+++ task-site/src/Components/Dialog/Dialog.js
@@ -0,0 +1,18 @@
+import "./Dialog.css"
+
+export default function Dialog({close, title, content}) {
+  return(
+    <div>
+      <div className="background" onClick={close} />
+      <div className="dialogContainer">
+        <div className="dialogHeader">
+          {title}
+          <div className="closeIcon" onClick={close}>X</div>
+        </div>
+        <div className="dialogContent">
+          {content}
+        </div>
+      </div>
+    </div>
+  )
+}
